8e25f4a9e40d634ce2e73be7b11d97ee7ed9775b
Shenjun Ma <mashenjun0902@gmail.com>
2015-12-02 15:57:06 +0100

    change the set of webview to disable the external browser

--- app/src/main/java/com/leanote/android/ui/post/BlogHomeActivity.java
+++ app/src/main/java/com/leanote/android/ui/post/BlogHomeActivity.java
@@ -1,20 +1,32 @@
 package com.leanote.android.ui.post;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebView;
-
-import com.leanote.android.R;
+import android.webkit.WebViewClient;
 
 public class BlogHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         //setContentView(R.layout.activity_blog_home);
         WebView webview = new WebView(this);
+
+        webview.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // TODO Auto-generated method stub
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
         webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("https://github.com/");
+        webview.loadUrl("http://www.google.com");
         setContentView(webview);
     }
 }
